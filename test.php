e203d701c0f00f964f08a7dddbea768a74991d4a
Joseph Ashwin <ashwinkjoseph@gmail.com>
2017-09-02 23:21:39 +0530

    changes

--- test.php
+++ test.php
@@ -16,10 +16,10 @@
 
 
 
-ini_set('SMTP', 'smtp.sendgrid.net');
-ini_set('smtp_port', 465);
-ini_set('username', 'apikey');
-ini_set('password', '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');
+// ini_set('SMTP', 'smtp.sendgrid.net');
+// ini_set('smtp_port', 465);
+// ini_set('username', 'apikey');
+// ini_set('password', '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');
 $headers = "From: miles@takshak.in";
         // boundary
         $semi_rand = md5(time());
@@ -30,14 +30,45 @@ $headers = "From: miles@takshak.in";
         $message_to .= "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $message_to . "\n\n";
         $message_to .= "--{$mime_boundary}\n";
 
+        $to = $email_new;
+        $subject = $subject_to;
+        $body = $message_to;
+        
+        $host = "smtp.sendgrid.net";
+        $port = "465";
+        $username = "apikey";
+        $password = "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";
+        $from = "traveller@takshak.in";
+        
+        $headers = array ('From' => $from,
+          'To' => $to,
+          'Subject' => $subject);
+        $smtp = Mail::factory('smtp',
+          array ('host' => $host,
+            'port' => $port,
+            'auth' => true,
+            'username' => $username,
+            'password' => $password));
+        
+        $mail = $smtp->send($to, $headers, $body);
+        
+        if (PEAR::isError($mail)) {
+          echo("
+       " . $mail->getMessage() . "
+       
+       ");  } else {   echo("
+       Message successfully sent!
+       
+       ");  } ?>
 
-$ok = mail($email_new, $subject_to, $message_to, $headers);
+<!-- $ok = mail($email_new, $subject_to, $message_to, $headers);
 
  if ($ok) {
                 echo "<p>mail sent to $to!</p>";
-$email_success=3;        }
+$email_success=3;
+}
 else{
         error_get_last();
-}
+} -->
 
-?>
+<!-- ?> -->
