73c363f492faf823d02d04b042c1b779539aa3c7
abhinavtk97 <abhinavtk97@gmail.com>
2018-07-13 10:28:05 +0530

    Update connectvars.php

--- connectvars.php
+++ connectvars.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST2325','ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DB_USER2325','hi623hpsz4cll2al');
-define('DB_PASSWORD2325','hcwd5drm3begpx91');
-define('DB_NAME2325','kc3zumqqccjyjlq3');
+define('');
+define('DB_USER2325','');
+define('DB_PASSWORD2325','');
+define('DB_NAME2325','');
 ?>
